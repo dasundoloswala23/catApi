1a4e748ff1175105370fa41dc8e64e73238e3e68
thari <tharindudasun0@gmail.com>
2024-08-02 19:56:01 +0530

    update the model

--- lib/controllers/imagesApi.dart
+++ lib/controllers/imagesApi.dart
@@ -1,20 +1,37 @@
-// apiController/imagesApi.dart
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../model/images.dart';
 
 class CatService {
   static const String _apiKey = 'live_lGGh383MzUSP9MdgqvAHcs3IC8CUfHyQnXQ4ySAmagb0uHSJQucWIto5pqfR2EZb';
-  static const String _url = 'https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=beng&api_key=$_apiKey';
+  static const String _baseUrl = 'https://api.thecatapi.com/v1/images/search';
 
-  static Future<List<Images>> fetchCats(int page, int limit) async {
-    final response = await http.get(Uri.parse(_url));
+  static Future<List<Images>> fetchCats(int page, int limit, String type) async {
+    String mimeTypes = type == 'images'
+        ? 'jpg,png'
+        : type == 'gifs'
+        ? 'gif'
+        : 'jpg,png,gif';
+
+    final url = '$_baseUrl?limit=$limit&page=$page&mime_types=$mimeTypes&order=random&api_key=$_apiKey';
+
+    print('Fetching cats from: $url');
+
+    final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
-      final List<Images> cats = data.map((json) => Images.fromJson(json)).toList();
+      print('Received data: $data');
+
+      final List<Images> cats = data
+          .where((item) =>
+      item['categories'] == null ||
+          !item['categories'].any((cat) => cat['name'] == 'hat'))
+          .map((json) => Images.fromJson(json))
+          .toList();
       return cats;
     } else {
+      print('Failed to load cats with status code: ${response.statusCode}');
       throw Exception('Failed to load cats');
     }
   }
